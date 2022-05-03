5bc51286c61bbe71cf023b1cc3f97e53422d8376
leo10ui <leonardo@leonardo.tec.br>
2022-05-03 15:43:19 -0300

    update

---
+++ 1° Semestre/Disciplinas Presenciais/Programacao Web Responsiva - Presencial/Exercícios/Lista 5/HTML, CSS, e JS separados/Exercício 1/Exercício-1.js
@@ -0,0 +1,3 @@
+var base = window.prompt("Digite a base do triângulo: ");
+var altura = window.prompt("Digite a altura do triângulo: ");
+var area = (base*altura)/2;
---
+++ 1° Semestre/Disciplinas Presenciais/Programacao Web Responsiva - Presencial/Exercícios/Lista 5/HTML, CSS, e JS separados/Exercício 4/Exercício-4.js
@@ -0,0 +1,16 @@
+var num_vezes = 1;
+var menor5 = 0;
+var maior5 = 0;
+while (num_vezes <= 10)
+{
+    var num_typed = window.prompt("Por favor, digite um número: ");
+    if(num_typed < 5)
+    {
+        menor5++;
+    }
+    else
+    {
+        maior5++;
+    }
+    num_vezes++;
+}
